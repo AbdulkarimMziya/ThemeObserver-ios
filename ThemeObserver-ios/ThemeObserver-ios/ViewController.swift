73cf8ad50e45c96684cb7338c8a680ddd61ca759
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2026-03-26 11:49:49 -0700

    Setup/programmatic UI (#1)
    * chore: modify project settings and remove storyboard references
    
    * setup: configure UIWindow and set rootViewController programmatically
    
    ---------
    
    Co-authored-by: Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>

---
+++ ThemeObserver-ios/ThemeObserver-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThemeObserver-ios
+//
+//  Created by Abdulkarim Mziya on 2026-03-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
